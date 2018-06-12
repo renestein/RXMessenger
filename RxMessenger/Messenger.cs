75a72cca68cf82c14577babe733b5cec9640d4bc
Rene Stein <rene@renestein.net>
2018-06-12 17:18:01 +0200

    Implementing Messnger

--- RxMessenger/Messenger.cs
+++ RxMessenger/Messenger.cs
@@ -1,7 +1,44 @@
-﻿namespace RxMessenger
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace RxMessenger
 {
-  public class Messenger
+  public class Messenger : IDisposable
   {
-    
+    private readonly Subject<Object> _events;
+    private bool _isDisposed;
+
+    public Messenger()
+    {
+      _events = new Subject<object>();
+      _isDisposed = false;
+    }
+
+    public void Dispose()
+    {
+      _events.Dispose();
+      _isDisposed = true;
+    }
+
+    public void PublishEvent<TEvent>(TEvent newEvent)
+    {
+      throwIfDisposed();
+      _events.OnNext(newEvent);
+    }
+
+    public IObservable<TEvent> GetEventStream<TEvent>()
+    {
+      throwIfDisposed();
+      return _events.OfType<TEvent>();
+    }
+
+    private void throwIfDisposed()
+    {
+      if (_isDisposed)
+      {
+        throw new ObjectDisposedException(GetType().FullName);
+      }
+    }
   }
 }
